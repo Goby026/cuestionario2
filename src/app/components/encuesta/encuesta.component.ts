aad6acf5ded9e5b4c45e1ae89344e21091618113
Grover <grendich@perudataconsult.net>
2018-12-03 20:31:39 -0500

    update from office

--- src/app/components/encuesta/encuesta.component.ts
+++ src/app/components/encuesta/encuesta.component.ts
@@ -1,88 +1,93 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {Component} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-encuesta',
-  templateUrl: './encuesta.component.html',
-  styleUrls: ['./encuesta.component.css']
+    selector: 'app-encuesta',
+    templateUrl: './encuesta.component.html',
+    styleUrls: ['./encuesta.component.css']
 })
-export class EncuestaComponent{
+export class EncuestaComponent {
 
-  cliente:string = "";
-  id:string = "";
+    cliente: string = '';
+    id: string = '';
 
-  forma:FormGroup;
+    forma: FormGroup;
 
-  uno:any = 1;
+    uno: any = 1;
 
-  encuesta:Object={
-    pregunta1:{
-      opcion1:0,
-      opcion2:0,
-      opcion3:0,
-      opcion4:0
-    },
-    pregunta2:{
-      opcion1:"",
-      opcion2:"",
-      opcion3:"",
-      opcion4:""
-    },
-    pregunta3:{
-      opcion1:"",
-      opcion2:"",
-      opcion3:"",
-      opcion4:""
-    },
-    pregunta4:{
-      opcion1:"",
-      opcion2:"",
-      opcion3:"",
-      opcion4:""
-    },
-    pregunta5:{
-      opcion1:"",
-      opcion2:"",
-      opcion3:"",
-      opcion4:""
-    },
-    id:""
-  }
+    encuesta: Object = {
+        pregunta1: {
+            id: '',
+            opcion: ''
+        },
+        pregunta2: {
+            id: '',
+            opcion: ''
+        },
+        pregunta3: {
+            opcion1: '',
+            opcion2: '',
+            opcion3: '',
+            opcion4: ''
+        },
+        pregunta4: {
+            opcion1: '',
+            opcion2: '',
+            opcion3: '',
+            opcion4: ''
+        },
+        pregunta5: {
+            opcion1: '',
+            opcion2: '',
+            opcion3: '',
+            opcion4: ''
+        },
+        id: ''
+    }
 
-  marcas:any[] = [
-    {id:"lenovo", nombre: "Lenovo"},
-    {id:"huawei", nombre: "Huawei"},
-    {id:"asus", nombre: "ASUS"},
-    {id:"msi", nombre: "MSI"},
-    {id:"hp", nombre: "HP"},
-    {id:"dell", nombre: "DELL"},
-    {id:"apple", nombre: "Apple"},
-    {id:"acer", nombre: "Acer"}
-  ];
+    marcas: any[] = [
+        {id: 'lenovo', nombre: 'Lenovo'},
+        {id: 'huawei', nombre: 'Huawei'},
+        {id: 'asus', nombre: 'ASUS'},
+        {id: 'msi', nombre: 'MSI'},
+        {id: 'hp', nombre: 'HP'},
+        {id: 'dell', nombre: 'DELL'},
+        {id: 'apple', nombre: 'Apple'},
+        {id: 'acer', nombre: 'Acer'}
+    ];
 
-  constructor(private activatedRoute: ActivatedRoute){
-    this.activatedRoute.params.subscribe( data => {
-      this.cliente = data.nombre;
-      this.id = data.id;
+    dispositivos: any[] = [
+        {codi: '1', nombre: 'Tablet'},
+        {codi: '2', nombre: 'Laptop'},
+        {codi: '3', nombre: 'PC de escritorio'},
+        {codi: '4', nombre: 'Smartphone'}
+    ];
 
-      console.log("clienteID: "+this.id)
-    });
+    constructor(private activatedRoute: ActivatedRoute) {
+        this.activatedRoute.params.subscribe(data => {
+            this.cliente = data.nombre;
+            this.id = data.id;
+
+            console.log('clienteID: ' + this.id);
+        });
 
 
-    this.forma = new FormGroup({
-      'pregunta1' : new FormGroup({
-        'opcion1': new FormControl('', Validators.required),
-        'opcion2': new FormControl('', Validators.required),
-        'opcion3': new FormControl('', Validators.required),
-        'opcion4': new FormControl('', Validators.required)
-      })
-    });
-  }
+        this.forma = new FormGroup({
+            'pregunta1': new FormGroup({
+                'id': new FormControl('', Validators.required),
+                'opcion': new FormControl('', Validators.required)
+            }),
+            'pregunta2': new FormGroup({
+                'id': new FormControl('', Validators.required),
+                'opcion': new FormControl('', Validators.required)
+            })
+        });
+    }
 
-  guardarEncuesta(){
-     console.log(this.encuesta);
-    console.log(this.forma.get('pregunta1').value);
-  }
+    guardarEncuesta() {
+        console.log(this.encuesta);
+        console.log(this.forma.value);
+    }
 
 }
